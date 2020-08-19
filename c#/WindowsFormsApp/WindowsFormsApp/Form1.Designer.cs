1abe7b118d3b9f4864e4bccdf5c71d2d12ca6814
riversnails <ekgur120736@gmail.com>
2020-08-19 16:27:30 +0900

    c#study

--- c#/WindowsFormsApp/WindowsFormsApp/Form1.Designer.cs
+++ c#/WindowsFormsApp/WindowsFormsApp/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // button1
@@ -41,21 +44,66 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(497, 208);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(44, 16);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "test";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "test1",
+            "test2",
+            "test3"});
+            this.checkedListBox1.Location = new System.Drawing.Point(595, 182);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(132, 132);
+            this.checkedListBox1.TabIndex = 2;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "test1",
+            "test2",
+            "test3"});
+            this.comboBox1.Location = new System.Drawing.Point(878, 204);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(154, 20);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1143, 628);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- c#/WindowsFormsApp/WindowsFormsApp/Form1.cs
+++ c#/WindowsFormsApp/WindowsFormsApp/Form1.cs
@@ -13,12 +13,14 @@ namespace WindowsFormsApp
     public partial class Form1 : Form
     {
         int count = 0;
+        Button remember_button;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e) // 그냥 버튼
         {
             Button new_button = new Button()
             {
@@ -30,6 +32,41 @@ namespace WindowsFormsApp
             };
 
             this.Controls.Add(new_button);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) // 체크박스
+        {
+            string text = "false";
+            if(checkBox1.Checked) text = "true";
+
+            Button new_button = new Button()
+            {
+                Width = 75,
+                Height = 23,
+                Left = 150,
+                Top = 150,
+                Text = text
+            };
+
+            this.Controls.Remove(remember_button);
+            remember_button = new_button;
+            this.Controls.Add(new_button);
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e) // 체크박스 리스트
+        {
+            int index = checkedListBox1.SelectedIndex;
+            string item = checkedListBox1.SelectedItem.ToString();
+            Console.WriteLine(index + "/" + item + "이 선택됨");
+            bool s = checkedListBox1.GetItemChecked(index);
+            Console.WriteLine(s);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) // 콤보박스
+        {
+            int index = comboBox1.SelectedIndex;
+            string item = comboBox1.SelectedItem.ToString();
+            Console.WriteLine(index + "/" + item + "이 선택됨");
         }
     }
 }
